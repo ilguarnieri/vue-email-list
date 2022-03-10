019f931d7a39cd1dfaa09180cfbfbb9dbe8b2aac
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-03-10 02:29:11 +0100

    complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,23 +4,30 @@ const app = new Vue({
     data: {
         emails: [],
         numberEmail: '',
+        divAlert: false,
+        startEmail: true,
     },
 
     methods: {
         getEmail: function(){
             if(this.numberEmail == 0 || this.numberEmail > 20){
-                alert('c')
+                this.divAlert = !this.divAlert;
+            }else{
 
-            }else{   
-
+                this.startEmail = false;
                 for(let i = 0; i < this.numberEmail; i++){
-                    
+                                   
                     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                     .then(email => {
                         this.emails.push(email.data.response);
                     })
-                }                
+                }             
             }
+        },
+        restart: function(){
+            this.emails = [];
+            this.numberEmail = '';
+            this.startEmail = true;
         }
     }
 })
